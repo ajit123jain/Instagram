a179e4ba360ccb63ed80a2705536187926f5c63e
ajit123jain <ajitkumarjain123@gmail.com>
2019-03-10 13:38:11 +0530

    added hashtag for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   root to: 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show,:show,:update]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create,:show,:destroy]
+  get 'search' => 'search#index'
 end
