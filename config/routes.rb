98125dec2d685ba31df7bb330d8b37fc5e93fdc1
ajit123jain <ajitkumarjain123@gmail.com>
2019-03-10 12:18:51 +0530

    post added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show,:show,:update]
+  resources :posts, only: [:new, :create]
 end
